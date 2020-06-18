a3bc663c05fdbf734e24016af97230ea0fa38dfe
Nirajan karki <karkinirajan199@gmail.com>
2020-06-18 22:24:46 +0545

    Done with the modalSerializers which is exact same as the modelforms

--- djangorest/restdjango/serializers.py
+++ djangorest/restdjango/serializers.py
@@ -2,20 +2,29 @@ from rest_framework import serializers
 from .models import Article
 
 
-class articleSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=200)
-    author = serializers.CharField(max_length=100)
-    email = serializers.EmailField(max_length=100)
-    date = serializers.DateTimeField()
+# class articleSerializer(serializers.Serializer):
+#     title = serializers.CharField(max_length=200)
+#     author = serializers.CharField(max_length=100)
+#     email = serializers.EmailField(max_length=100)
+#     date = serializers.DateTimeField()
 
-    def create(self, validated_data):
-        return Article.objects.create(**validated_data)
+#     def create(self, validated_data):
+#         return Article.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
+#     def update(self, instance, validated_data):
 
-        instance.title = validated_data.get("title", instance.title)
-        instance.author = validated_data.get("author", instance.author)
-        instance.email = validated_data.get("email", instance.email.lower)
-        instance.date = validated_data.get("date", instance.date)
-        instance.save()
-        return instance
+#         instance.title = validated_data.get("title", instance.title)
+#         instance.author = validated_data.get("author", instance.author)
+#         instance.email = validated_data.get("email", instance.email.lower)
+#         instance.date = validated_data.get("date", instance.date)
+#         instance.save()
+#         return instance
+
+# REPLACING THE OBOVE ENTIRE CODE WITH MODELSERIALIZER
+
+
+class artileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ["id", "title", "author", "email"]
+
